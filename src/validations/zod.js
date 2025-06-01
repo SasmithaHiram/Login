5ce86fb326cb1e15f3987ec988cee3f702b4e7f1
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-01 21:24:28 +0530

    Validation - Config Zod

---
+++ src/validations/zod.js
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  firstName: z.string
+    .min(1, "First name is required")
+    .max(20, "First name must be less than 20 characters"),
+  lastName: z.string
+    .min(1, "Last name is required")
+    .max(20, "Last name must be less than 20 characters"),
+  userName: z.string
+    .min(1, "Username is required")
+    .max(20, "Username must be less than 20 characters"),
+  password: z.string
+    .min(1, "Password is required")
+    .max(20, "Password must be less than 20 characters"),
+});
